7be0d5f7e058732509453d8299dcd65e9b415245
Eduard Micloș <59684906+EduardMiclos@users.noreply.github.com>
2021-05-03 15:27:09 +0300

    Update main.py
    Updatat main-ul.

--- Distribuții/main.py
+++ Distribuții/main.py
@@ -1,8 +1,9 @@
 from Bernoulli import Bern
-from Uniforma import Unif
+from Uniform import Unif
 from Geometric import Geom
 from Exponential import Exp
 from NonUniform import NonUnif
+from Binomial import Bin
 import sys
 
 # types of distributions:
@@ -12,25 +13,31 @@ import sys
 #   3 - Exponential -> one parameter, theta (float, strictly positive)
 #   4 - NonUniform -> 2*k parameters, where k is the number of values
 #   and also the number of probabilities: x1 x2 ... xk p1 p2 ... pk
-# command line arguments:  iterations, type_of_distribution, parameters
+#   5 - Binomial -> k + 1 parameters: the values x1 x2 ... xk and the
+#   probability associated to a Bernoulli distribution (thus, the other probabilities
+#   can be deduced using the formula from the textbook)
+#   command line arguments:  iterations type_of_distribution parameters
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         itr, type = [int(argc) for argc in sys.argv[1:3]]
         params = [float(argc) for argc in sys.argv[3:]]
-        distrbutions = [Bern, Unif, Geom, Exp, NonUnif]
+        distrbutions = [Bern, Unif, Geom, Exp, NonUnif, Bin]
 
         distr = distrbutions[type](*params)
-        for i in range(itr):
-            print(distr.simulate())
+        #for i in range(itr):
+            #print(distr.simulate())
 
-        # FOR TESTING PURPOSES ( WILL DELETE SOON )
+        #FOR TESTING PURPOSES ( WILL DELETE SOON )
         # import numpy as np
-        # X = [-4, -2, 10]
+        # X = [5, 10, 14]
         # Y = np.zeros(1000, dtype=int)
         #
         # for i in range(itr):
         #     Y[distr.simulate()]+=1
+        #
+        # #distr.simulate()
         #
         # for x in X:
         #     print(x, 'apare de:', Y[x], 'ori')
